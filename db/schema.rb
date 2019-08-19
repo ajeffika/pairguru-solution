968cb2e5d16fc9a976c1231b4117c733cee401df
ajeffika <jan.mistarz6767@gmail.com>
2019-08-20 01:08:43 +0200

    resolved conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_08_19_183829) do
 
   create_table "comments", force: :cascade do |t|
@@ -25,8 +24,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_183829) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "movies", force: :cascade do |t|
@@ -35,8 +34,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_183829) do
     t.datetime "released_at"
     t.string "avatar"
     t.integer "genre_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["genre_id"], name: "index_movies_on_genre_id"
   end
 
@@ -51,8 +50,8 @@ ActiveRecord::Schema.define(version: 2019_08_19_183829) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "phone_number"
     t.string "confirmation_token"
     t.datetime "confirmed_at"
